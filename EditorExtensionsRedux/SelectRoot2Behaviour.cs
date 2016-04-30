0cbd2fcbfd708dac3e89583f48650b5df2cceae8
Jonathan Bayer <linuxgurugamer@gmail.com>
2016-04-29 23:13:35 -0400

    Fixed nullrefs

--- EditorExtensionsRedux/SelectRoot2Behaviour.cs
+++ EditorExtensionsRedux/SelectRoot2Behaviour.cs
@@ -26,7 +26,7 @@ namespace EditorExtensionsRedux.SelectRoot2 {
 		const int ST_ROOT_SELECT = 80;
 		const int ST_ROOT_UNSELECTED = 79;
 		const int MODEMSG = 63;
-		const int ST_IDLE = 70;
+		const int ST_IDLE = 73;
 		const int ST_PLACE = 74;
 		const int ONMOUSEISOVER = 250;
 		const int GET_STATEEVENTS = 0;
@@ -51,112 +51,108 @@ namespace EditorExtensionsRedux.SelectRoot2 {
 			// Oh god, so much dirty reflection. Please don't sue me, Squad :(
 			//KerbalFSM editorFSM = (KerbalFSM)Refl.GetValue(EditorLogic.fetch, "\u0001");
 
-			{
-				// Skip first click in root selection mode:
-				KFSMEvent skipFirstClickEvent = new KFSMEvent("SelectRoot2_skipFirstClickEvent");
-				skipFirstClickEvent.OnCheckCondition = (state) => {
-					// Um about that always true condition... There is a funny sound glitch
-					// if the root part doesn't have any ROOTABLE children so check for that.
-					// Thanks Kerbas_ad_astra, http://forum.kerbalspaceprogram.com/threads/43208?p=1948755#post1948755
-					return EditorReRootUtil.GetRootCandidates(EditorLogic.RootPart.GetComponentsInChildren<Part>()).Any();
-				};
+
+			// Skip first click in root selection mode:
+			KFSMEvent skipFirstClickEvent = new KFSMEvent("SelectRoot2_skipFirstClickEvent");
+			skipFirstClickEvent.OnCheckCondition = (state) => {
+				// Um about that always true condition... There is a funny sound glitch
+				// if the root part doesn't have any ROOTABLE children so check for that.
+				// Thanks Kerbas_ad_astra, http://forum.kerbalspaceprogram.com/threads/43208?p=1948755#post1948755
+				return EditorReRootUtil.GetRootCandidates(EditorLogic.RootPart.GetComponentsInChildren<Part>()).Any();
+			};
 
 
-				skipFirstClickEvent.OnEvent = () => {
-					//Refl.SetValue(EditorLogic.fetch, "selectedPart", EditorLogic.RootPart); // SelectedPart
-					Refl.SetValue(EditorLogic.fetch, SELECTEDPART, EditorLogic.RootPart); // SelectedPart
-				};
-				//KFSMState st_root_select = (KFSMState)Refl.GetValue(EditorLogic.fetch, "st_root_select");
-				KFSMState st_root_select = (KFSMState)Refl.GetValue(EditorLogic.fetch, ST_ROOT_SELECT);
-				skipFirstClickEvent.GoToStateOnEvent = st_root_select;
+			skipFirstClickEvent.OnEvent = () => {
+				//Refl.SetValue(EditorLogic.fetch, "selectedPart", EditorLogic.RootPart); // SelectedPart
+				Refl.SetValue(EditorLogic.fetch, SELECTEDPART, EditorLogic.RootPart); // SelectedPart
+			};
+			//KFSMState st_root_select = (KFSMState)Refl.GetValue(EditorLogic.fetch, "st_root_select");
+			KFSMState st_root_select = (KFSMState)Refl.GetValue(EditorLogic.fetch, ST_ROOT_SELECT);
+			skipFirstClickEvent.GoToStateOnEvent = st_root_select;
 
+			//KFSMState st_root_unselected = (KFSMState)Refl.GetValue(EditorLogic.fetch, "st_root_unselected");
+			KFSMState st_root_unselected = (KFSMState)Refl.GetValue (EditorLogic.fetch, ST_ROOT_UNSELECTED);
+			InjectEvent (st_root_unselected, skipFirstClickEvent);
 
-				//KFSMState st_root_unselected = (KFSMState)Refl.GetValue(EditorLogic.fetch, "st_root_unselected");
-				KFSMState st_root_unselected = (KFSMState)Refl.GetValue (EditorLogic.fetch, ST_ROOT_UNSELECTED);
-				InjectEvent (st_root_unselected, skipFirstClickEvent);
-
-				// Fix ability to select if already hovering:
-				KFSMStateChange fixAlreadyHoveringPartFn = (from) => {
-					Part partUnderCursor = GetPartUnderCursor();
-					var selectors = EditorLogic.SortedShipList;
-				
-					//EditorLogic.fetch.Lock (true, true, true, "SelectRoot2");
-
-					var selectorUnderCursor = selectors.Find(x => (Part)x == partUnderCursor);
-					if(selectorUnderCursor) 
-					{
-//						Refl.Invoke(selectorUnderCursor, "OnMouseIsOver");
-						Refl.Invoke(selectorUnderCursor, ONMOUSEISOVER);
-					}
-				};
-				st_root_select.OnEnter += fixAlreadyHoveringPartFn;
-				OnCleanup += () => {
-					Log.Info("SelectRoot OnCleanup 1");
-					st_root_select.OnEnter -= fixAlreadyHoveringPartFn;
-				};
-
-				// Provide a more meaningful message after our changes:
-				KFSMStateChange postNewMessageFn = (from) => {
-					//var template = (ScreenMessage)Refl.GetValue(EditorLogic.fetch, "modeMsg");
-					var template = (ScreenMessage)Refl.GetValue(EditorLogic.fetch, MODEMSG);
-					ScreenMessages.PostScreenMessage("Select a new root part", template);
-				};
-				st_root_select.OnEnter += postNewMessageFn;
-				OnCleanup += () => {
-					Log.Info("SelectRoot OnCleanup 2");
-					st_root_select.OnEnter -= postNewMessageFn;
-				};
-			}
-#if true
+			// Fix ability to select if already hovering:
+			KFSMStateChange fixAlreadyHoveringPartFn = (from) => {
+				Part partUnderCursor = GetPartUnderCursor();
+				var selectors = EditorLogic.SortedShipList;
 			
+				//EditorLogic.fetch.Lock (true, true, true, "SelectRoot2");
+
+				var selectorUnderCursor = selectors.Find(x => (Part)x == partUnderCursor);
+				if(selectorUnderCursor) 
+				{
+//						Refl.Invoke(selectorUnderCursor, "OnMouseIsOver");
+					Refl.Invoke(selectorUnderCursor, ONMOUSEISOVER);
+				}
+			};
+			st_root_select.OnEnter += fixAlreadyHoveringPartFn;
+			OnCleanup += () => {
+				Log.Info("SelectRoot OnCleanup 1");
+				st_root_select.OnEnter -= fixAlreadyHoveringPartFn;
+			};
+
+			// Provide a more meaningful message after our changes:
+			KFSMStateChange postNewMessageFn = (from) => {
+				//var template = (ScreenMessage)Refl.GetValue(EditorLogic.fetch, "modeMsg");
+				var template = (ScreenMessage)Refl.GetValue(EditorLogic.fetch, MODEMSG);
+				ScreenMessages.PostScreenMessage("Select a new root part", template);
+			};
+
+			st_root_select.OnEnter += postNewMessageFn;
+			OnCleanup += () => {
+				Log.Info("SelectRoot OnCleanup 2");
+				st_root_select.OnEnter -= postNewMessageFn;
+			};
+
 			// Drop the new root part after selection:
-			{
-				KFSMEvent dropNewRootPartEvent = new KFSMEvent("SelectRoot2_dropNewRootPartEvent");
-				dropNewRootPartEvent.OnCheckCondition = (state) => {
-					return EditorLogic.fetch.lastEventName.Equals("on_rootSelect");
-				};
-				dropNewRootPartEvent.OnEvent = () => {
-					// Normally the backup is triggered in on_partDropped#OnCheckCondition()
-					// But we skip that, so do backup here instead.
 
-					EditorLogic.fetch.SetBackup();
-					// Normally triggered in on_partDropped#OnEvent().
-					GameEvents.onEditorPartEvent.Fire(ConstructionEventType.PartDropped, EditorLogic.SelectedPart);
-					//var template = (ScreenMessage)Refl.GetValue(EditorLogic.fetch, "modeMsg");
-					var template = (ScreenMessage)Refl.GetValue(EditorLogic.fetch, MODEMSG);
-					//ScreenMessages.PostScreenMessage(String.Empty, template);
-					if (template != null)
-						ScreenMessages.PostScreenMessage("New Root selected and dropped", template);
-					else
-						ScreenMessages.PostScreenMessage("New Root selected and dropped");
+			KFSMEvent dropNewRootPartEvent = new KFSMEvent("SelectRoot2_dropNewRootPartEvent");
+			dropNewRootPartEvent.OnCheckCondition = (state) => {
+				return EditorLogic.fetch.lastEventName.Equals("on_rootSelect");
+			};
 
-					EditorLogic.SelectedPart.gameObject.SetLayerRecursive(0, 1 << 21);
+			dropNewRootPartEvent.OnEvent = () => {
+				// Normally the backup is triggered in on_partDropped#OnCheckCondition()
+				// But we skip that, so do backup here instead.
+
+				EditorLogic.fetch.SetBackup();
+				// Normally triggered in on_partDropped#OnEvent().
+				GameEvents.onEditorPartEvent.Fire(ConstructionEventType.PartDropped, EditorLogic.SelectedPart);
+				//var template = (ScreenMessage)Refl.GetValue(EditorLogic.fetch, "modeMsg");
+				var template = (ScreenMessage)Refl.GetValue(EditorLogic.fetch, MODEMSG);
+				//ScreenMessages.PostScreenMessage(String.Empty, template);
+				if (template != null)
+					ScreenMessages.PostScreenMessage("New Root selected and dropped", template);
+				else
+					ScreenMessages.PostScreenMessage("New Root selected and dropped");
+
+				EditorLogic.SelectedPart.gameObject.SetLayerRecursive(0, 1 << 21);
 #if false
-					
-					Part[] parts = EditorLogic.RootPart.GetComponentsInChildren<Part>();
-					foreach (var p in parts)
-					{
-						Log.Info(p.ToString() + "p.enabled: " + p.enabled.ToString());
+				
+				Part[] parts = EditorLogic.RootPart.GetComponentsInChildren<Part>();
+				foreach (var p in parts)
+				{
+					Log.Info(p.ToString() + "p.enabled: " + p.enabled.ToString());
 
-						//Log.Info(p.ToString() + "p.frozen: " + p.active .ToString());
-					}
+					//Log.Info(p.ToString() + "p.frozen: " + p.active .ToString());
+				}
 #endif
-					//EditorLogic.fetch.Unlock("SelectRoot2");
-				};
+				//EditorLogic.fetch.Unlock("SelectRoot2");
+			};
 
-				// problem is with the following line
-				//KFSMState st_idle = (KFSMState)Refl.GetValue(EditorLogic.fetch, "st_idle");
-				KFSMState st_idle = (KFSMState)Refl.GetValue(EditorLogic.fetch, ST_IDLE);
-				dropNewRootPartEvent.GoToStateOnEvent = st_idle;
+			// problem is with the following line
+			//KFSMState st_idle = (KFSMState)Refl.GetValue(EditorLogic.fetch, "st_idle");
+			KFSMState st_idle = (KFSMState)Refl.GetValue(EditorLogic.fetch, ST_IDLE);
+			dropNewRootPartEvent.GoToStateOnEvent = st_idle;
 
+			//KFSMState st_place = (KFSMState)Refl.GetValue(EditorLogic.fetch, "st_place");
+			KFSMState st_place = (KFSMState)Refl.GetValue(EditorLogic.fetch, ST_PLACE);
+			InjectEvent(st_place, dropNewRootPartEvent);
+				
 
-				//KFSMState st_place = (KFSMState)Refl.GetValue(EditorLogic.fetch, "st_place");
-				KFSMState st_place = (KFSMState)Refl.GetValue(EditorLogic.fetch, ST_PLACE);
-				InjectEvent(st_place, dropNewRootPartEvent);
-					
-
-			}
-#endif
 			Log.Info("Setup complete..");
 		}
 
@@ -176,7 +172,7 @@ namespace EditorExtensionsRedux.SelectRoot2 {
 			OnCleanup += () => {
 //				((List<KFSMEvent>)Refl.GetValue(state, "stateEvents")).Remove(injectedEvent);
 				Log.Info("SelectRoot OnCleanup 3");
-				List<KFSMState> kfsmstatelist = (List<KFSMState>)Refl.GetValue(state, GET_STATEEVENTS);
+//				List<KFSMState> kfsmstatelist = (List<KFSMState>)Refl.GetValue(state, GET_STATEEVENTS);
 #if false
 				foreach(var kfsmstate in kfsmstatelist)
 				{
--- EditorExtensionsRedux/deploy.bat
+++ EditorExtensionsRedux/deploy.bat
@@ -1,16 +1,16 @@
 
 
-mkdir R:\KSP_1.0.5_Dev\GameData\EditorExtensionsRedux
-mkdir R:\KSP_1.0.5_Dev\GameData\EditorExtensionsRedux\Textures
-mkdir R:\KSP_1.0.5_Dev\GameData\EditorExtensionsRedux\PluginData
-mkdir R:\KSP_1.0.5_Dev\GameData\EditorExtensionsRedux\PluginData\StripSymmetry
+mkdir R:\KSP_1.1.1_dev\GameData\EditorExtensionsRedux
+mkdir R:\KSP_1.1.1_dev\GameData\EditorExtensionsRedux\Textures
+mkdir R:\KSP_1.1.1_dev\GameData\EditorExtensionsRedux\PluginData
+mkdir R:\KSP_1.1.1_dev\GameData\EditorExtensionsRedux\PluginData\StripSymmetry
 
 
-copy /Y "%~dp0bin\Debug\EditorExtensionsRedux.dll" "R:\KSP_1.0.5_Dev\GameData\EditorExtensionsRedux"
-copy /Y "%~dp0bin\Debug\Textures\*.png" "R:\KSP_1.0.5_Dev\GameData\EditorExtensionsRedux\Textures"
-copy /Y "%~dp0bin\Debug\EditorExtensionsRedux.version" "R:\KSP_1.0.5_Dev\GameData\EditorExtensionsRedux"
+copy /Y "%~dp0bin\Debug\EditorExtensionsRedux.dll" "R:\KSP_1.1.1_dev\GameData\EditorExtensionsRedux"
+copy /Y "%~dp0bin\Debug\Textures\*.png" "R:\KSP_1.1.1_dev\GameData\EditorExtensionsRedux\Textures"
+copy /Y "%~dp0bin\Debug\EditorExtensionsRedux.version" "R:\KSP_1.1.1_dev\GameData\EditorExtensionsRedux"
 
-copy /Y "%~dp0bin\Debug\License.txt" "R:\KSP_1.0.5_Dev\GameData\EditorExtensionsRedux"
-copy /Y "%~dp0bin\Debug\README.md" "R:\KSP_1.0.5_Dev\GameData\EditorExtensionsRedux"
+copy /Y "%~dp0bin\Debug\License.txt" "R:\KSP_1.1.1_dev\GameData\EditorExtensionsRedux"
+copy /Y "%~dp0bin\Debug\README.md" "R:\KSP_1.1.1_dev\GameData\EditorExtensionsRedux"
 cd
-copy /y "StripSymmetry\Gamedata\StripSymmetry\plugins\PluginData\StripSymmetry\config.xml"  "R:\KSP_1.0.5_Dev\GameData\EditorExtensionsRedux\PluginData\StripSymmetry"
+copy /y "StripSymmetry\Gamedata\StripSymmetry\plugins\PluginData\StripSymmetry\config.xml"  "R:\KSP_1.1.1_dev\GameData\EditorExtensionsRedux\PluginData\StripSymmetry"
