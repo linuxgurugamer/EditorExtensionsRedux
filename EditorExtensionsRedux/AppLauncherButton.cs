2e441102ed171dcef60f4b5fb20f54c12bbc4cee
Jonathan Bayer <linuxgurugamer@gmail.com>
2021-07-27 19:58:31 -0400

    Added AssemblyFileVersion  Updated version file for 1.12  Removed some whitespace (formatting issues in code only)

--- EditorExtensionsRedux/AppLauncherButton.cs
+++ EditorExtensionsRedux/AppLauncherButton.cs
@@ -47,72 +47,12 @@ namespace EditorExtensionsRedux
         /// Copied from ToolbarController
         /// </summary>
         /// 
-              //
-        // The following function was initially copied from @JPLRepo's AmpYear mod, which is covered by the GPL, as is this mod
-        //
-        // This function will attempt to load either a PNG or a JPG from the specified path.  
-        // It first checks to see if the actual file is there, if not, it then looks for either a PNG or a JPG
-        //
-        // easier to specify different cases than to change case to lower.  This will fail on MacOS and Linux
-        // if a suffix has mixed case
-        static string[] imgSuffixes = new string[] { ".png", ".jpg", ".gif", ".PNG", ".JPG", ".GIF" };
-        public static Boolean LoadImageFromFile(ref Texture2D tex, String fileNamePath)
-        {
-
-            Boolean blnReturn = false;
-            try
-            {
-                string path = fileNamePath;
-                if (!System.IO.File.Exists(fileNamePath))
-                {
-                    // Look for the file with an appended suffix.
-                    for (int i = 0; i < imgSuffixes.Length; i++)
-
-                        if (System.IO.File.Exists(fileNamePath + imgSuffixes[i]))
-                        {
-                            path = fileNamePath + imgSuffixes[i];
-                            break;
-                        }
-                }
-
+        //        // The following function was initially copied from @JPLRepo's AmpYear mod, which is covered by the GPL, as is this mod        //        // This function will attempt to load either a PNG or a JPG from the specified path.          // It first checks to see if the actual file is there, if not, it then looks for either a PNG or a JPG        //        // easier to specify different cases than to change case to lower.  This will fail on MacOS and Linux        // if a suffix has mixed case
+        static string[] imgSuffixes = new string[] { ".png", ".jpg", ".gif", ".PNG", ".JPG", ".GIF" };        public static Boolean LoadImageFromFile(ref Texture2D tex, String fileNamePath)        {            Boolean blnReturn = false;            try            {                string path = fileNamePath;                if (!System.IO.File.Exists(fileNamePath))                {                    // Look for the file with an appended suffix.                    for (int i = 0; i < imgSuffixes.Length; i++)                        if (System.IO.File.Exists(fileNamePath + imgSuffixes[i]))                        {                            path = fileNamePath + imgSuffixes[i];                            break;                        }                }
                 //File Exists check
-                if (System.IO.File.Exists(path))
-                {
-                    try
-                    {
-                        tex.LoadImage(System.IO.File.ReadAllBytes(path));
-                        blnReturn = true;
-                    }
-                    catch (Exception ex)
-                    {
-                        Log.Error("Failed to load the texture:" + path);
-                        Log.Error(ex.Message);
-                    }
-                }
-                else
-                {
-                    Log.Error("Cannot find texture to load:" + fileNamePath);
-                }
-
-
-            }
-            catch (Exception ex)
-            {
-                Log.Error("Failed to load (are you missing a file):" + fileNamePath);
-                Log.Error(ex.Message);
-            }
-            return blnReturn;
-        }
-
-        Texture2D GetTexture(string path, bool b)
-        {
-
-            Texture2D tex = new Texture2D(16, 16, TextureFormat.ARGB32, false);
-
-            if (LoadImageFromFile(ref tex, KSPUtil.ApplicationRootPath + "GameData/" + path))
-                return tex;
-            return tex;
-        }
+                if (System.IO.File.Exists(path))                {                    try                    {                        tex.LoadImage(System.IO.File.ReadAllBytes(path));                        blnReturn = true;                    }                    catch (Exception ex)                    {                        Log.Error("Failed to load the texture:" + path);                        Log.Error(ex.Message);                    }                }                else                {                    Log.Error("Cannot find texture to load:" + fileNamePath);                }            }            catch (Exception ex)            {                Log.Error("Failed to load (are you missing a file):" + fileNamePath);                Log.Error(ex.Message);            }            return blnReturn;        }
+        Texture2D GetTexture(string path, bool b)        {            Texture2D tex = new Texture2D(16, 16, TextureFormat.ARGB32, false);
+            if (LoadImageFromFile(ref tex, KSPUtil.ApplicationRootPath + "GameData/" + path))                return tex;            return tex;        }
 /// <summary>
 /// End of copy
 /// </summary>
